563accf39909f4f6a514c9bee7ca43c27a876ca3
twaldin <timothy@waldin.net>
2025-08-16 15:25:12 -0700

    timothy instead of tim

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -5,7 +5,7 @@ source "$(dirname "$0")/shared-functions.sh"
 
 # Top separator
 # ASCII header with progressive typewriter display
-ascii_typewriter "tim waldin" "Univers" "${BOLD}${CYAN}"
+ascii_typewriter "timothy waldin" "Univers" "${BOLD}${CYAN}"
 animated_separator "═" 20
 typewriter "${DIM}Type \"help\" for a list of available commands.${RESET}"
 typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
