c73bc3ce845b88600569aa0e6940603246323c96
twaldin <timothy@waldin.net>
2025-08-16 14:04:25 -0700

    fix scripts

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -16,9 +16,9 @@ echo ""
 typewriter "${BOLD}${WHITE} links${RESET}"
 typewriter "${CYAN}󰇮 \e]8;;mailto:tim@waldin.net\e\\tim@waldin.net\e]8;;\e\\${RESET}   ${YELLOW} \e]8;;https://github.com/twaldin\e\\github\e]8;;\e\\${RESET}   ${MAGENTA} \e]8;;https://linkedin.com/in/twaldin\e\\linkedin\e]8;;\e\\${RESET}    ${GREEN}󰋾 \e]8;;https://instagram.com/timn.w\e\\instagram\e]8;;\e\\${RESET}"
 
-typewriter "${DIM}Type \"help\" for a lit of available commands.${RESET}"
+typewriter "${DIM}Type \"help\" for a list of available commands.${RESET}"
 typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
-typewritter "${DIM}Type \"projects\" to see my projects.${RESET}"
+typewriter "${DIM}Type \"projects\" to see my projects.${RESET}"
 echo ""
 
 animated_separator "═" 139
