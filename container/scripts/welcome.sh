421834b0bbe19000c423d06058059c211a22d9c7
twaldin <timothy@waldin.net>
2025-08-16 15:24:26 -0700

    removed some wrappers, made blog use shared functions

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -4,10 +4,9 @@
 source "$(dirname "$0")/shared-functions.sh"
 
 # Top separator
-animated_separator "═" 139
 # ASCII header with progressive typewriter display
 ascii_typewriter "tim waldin" "Univers" "${BOLD}${CYAN}"
+animated_separator "═" 20
 typewriter "${DIM}Type \"help\" for a list of available commands.${RESET}"
 typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
 typewriter "${DIM}Type \"projects\" to see my projects.${RESET}"
-animated_separator "═" 139
