0a607aa45f2f8c237369d35f14da7d4c0d627957
twaldin <timothy@waldin.net>
2025-08-16 13:43:50 -0700

    fix message

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -4,7 +4,7 @@
 source "$(dirname "$0")/shared-functions.sh"
 
 # Don't clear - keep the typed "welcome" command visible
-echo ""  # Just add a newline after the command
+echo "" # Just add a newline after the command
 
 # Top separator
 animated_separator "═" 139
@@ -14,13 +14,11 @@ ascii_typewriter "tim waldin" "Univers" "${BOLD}${CYAN}"
 
 echo ""
 typewriter "${BOLD}${WHITE} links${RESET}"
-typewriter "${CYAN}󰇮 tim@waldin.net${RESET}   ${YELLOW} https://github.com/twaldin${RESET}   ${MAGENTA} https://linkedin.com/in/twaldin${RESET}    ${GREEN}󰋾 https://instagram.com/timn.w${RESET}"
+typewriter "${CYAN}󰇮 \e]8;;mailto:tim@waldin.net\e\\tim@waldin.net\e]8;;\e\\${RESET}   ${YELLOW} \e]8;;https://github.com/twaldin\e\\github\e]8;;\e\\${RESET}   ${MAGENTA} \e]8;;https://linkedin.com/in/twaldin\e\\linkedin\e]8;;\e\\${RESET}    ${GREEN}󰋾 \e]8;;https://instagram.com/timn.w\e\\instagram\e]8;;\e\\${RESET}"
 
-# Welcome message with typewritertypewriter "${DIM}You can explore my projects and this filesystem using all normal tools (eg. cd, ls, fzf, nvim, etc.${RESET}"
 typewriter "${DIM}Type \"help\" for a lit of available commands.${RESET}"
 typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
 typewritter "${DIM}Type \"projects\" to see my projects.${RESET}"
 echo ""
 
 animated_separator "═" 139
-
