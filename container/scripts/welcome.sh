dbd62ef1948978a283364b521316650a64a02b5e
twaldin <timothy@waldin.net>
2025-08-16 13:33:06 -0700

    fixed scripts and added welcome typing

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -3,29 +3,24 @@
 # Source shared functions
 source "$(dirname "$0")/shared-functions.sh"
 
-clear
+# Don't clear - keep the typed "welcome" command visible
+echo ""  # Just add a newline after the command
 
 # Top separator
 animated_separator "═" 139
 
 # ASCII header with progressive typewriter display
-ascii_typewriter "twald.in" "Univers" "${BOLD}${CYAN}"
+ascii_typewriter "tim waldin" "Univers" "${BOLD}${CYAN}"
 
 echo ""
-typewriter "${CYAN}󰇮 tim@waldin.net${RESET}"
-typewriter "${YELLOW} https://github.com/twaldin${RESET}"
-typewriter "${MAGENTA} https://linkedin.com/in/twaldin${RESET}"
-typewriter "${GREEN}󰋾 https://instagram.com/timn.w${RESET}"
+typewriter "${BOLD}${WHITE} links${RESET}"
+typewriter "${CYAN}󰇮 tim@waldin.net${RESET}   ${YELLOW} https://github.com/twaldin${RESET}   ${MAGENTA} https://linkedin.com/in/twaldin${RESET}    ${GREEN}󰋾 https://instagram.com/timn.w${RESET}"
 
-echo ""
-animated_separator "═" 139
-
-# Welcome message with typewriter
-echo ""
-typewriter "${GREEN}Welcome to twald.in terminal portfolio${RESET}"
-typewriter "${WHITE}This is a fully interactive ubuntu linux terminal hosting my projects and blog${RESET}"
-typewriter "${DIM}You can explore my projects and this filesystem using all normal tools (eg. cd, ls, fzf, nvim, etc.${RESET}"
-typewriter "${DIM}Type projects to see my projects - Type blog to see my blog - Type help to see all commands.${RESET}"
+# Welcome message with typewritertypewriter "${DIM}You can explore my projects and this filesystem using all normal tools (eg. cd, ls, fzf, nvim, etc.${RESET}"
+typewriter "${DIM}Type \"help\" for a lit of available commands.${RESET}"
+typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
+typewritter "${DIM}Type \"projects\" to see my projects.${RESET}"
 echo ""
 
 animated_separator "═" 139
+
