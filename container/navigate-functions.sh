6b80eafbd3b7bb075d5dcc4d3be8bb4e998a4ed0
twaldin <timothy@waldin.net>
2025-08-15 23:18:45 -0700

    fixed navigation

---
+++ container/navigate-functions.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+# Navigation functions for project exploration
+
+# Projects overview function
+projects() {
+    # Run the projects script to show overview
+    /home/portfolio/scripts/projects
+}
+
+# STM32 Games navigation function
+stm32-games() {
+    # Show project info
+    /home/portfolio/scripts/stm32-games
+    echo
+    echo -e "\033[38;5;227mNavigating to projects/stm32-games...\033[0m"
+    cd projects/stm32-games 2>/dev/null || {
+        echo -e "\033[38;5;210mError: Could not navigate to projects/stm32-games\033[0m"
+        echo -e "\033[38;5;117mTry: ls projects/\033[0m"
+        return 1
+    }
+}
+
+# Term Site navigation function
+term-site() {
+    # Show project info
+    /home/portfolio/scripts/term-site
+    echo
+    echo -e "\033[38;5;227mNavigating to projects/term-site...\033[0m"
+    cd projects/term-site 2>/dev/null || {
+        echo -e "\033[38;5;210mError: Could not navigate to projects/term-site\033[0m"
+        echo -e "\033[38;5;117mTry: ls projects/\033[0m"
+        return 1
+    }
+}
+
+# Sulfur Recipes navigation function
+sulfur-recipies() {
+    # Show project info
+    /home/portfolio/scripts/sulfur-recipies
+    echo
+    echo -e "\033[38;5;227mNavigating to projects/sulfur-recipies...\033[0m"
+    cd projects/sulfur-recipies 2>/dev/null || {
+        echo -e "\033[38;5;210mError: Could not navigate to projects/sulfur-recipies\033[0m"
+        echo -e "\033[38;5;117mTry: ls projects/\033[0m"
+        return 1
+    }
+}
+
+# Dotfiles navigation function
+dotfiles() {
+    # Show project info
+    /home/portfolio/scripts/dotfiles
+    echo
+    echo -e "\033[38;5;227mNavigating to projects/dotfiles...\033[0m"
+    cd projects/dotfiles 2>/dev/null || {
+        echo -e "\033[38;5;210mError: Could not navigate to projects/dotfiles\033[0m"
+        echo -e "\033[38;5;117mTry: ls projects/\033[0m"
+        return 1
+    }
+}
+
+# Home navigation function
+home() {
+    echo -e "\033[38;5;227mNavigating to home directory...\033[0m"
+    cd /home/portfolio
+    /home/portfolio/scripts/welcome.sh
+}
+
+# Welcome alias for home
+welcome() {
+    home
+}
+
+# Export functions so they're available in the shell
+export -f projects stm32-games term-site sulfur-recipies dotfiles home welcome
