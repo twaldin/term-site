53f8f394a4f8f6e0d62a777ab44263f9358c9a62
twaldin <timothy@waldin.net>
2025-08-18 01:53:52 -0700

    simple welcome script

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -8,6 +8,5 @@ clear
 # ASCII header with progressive typewriter display
 ascii_typewriter "timothy waldin" "Univers" "${BOLD}${CYAN}"
 typewriter "${DIM}Type \"help\" for a list of available commands.${RESET}"
-typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
-typewriter "${DIM}Type \"projects\" to see my projects.${RESET}"
+
 echo""
