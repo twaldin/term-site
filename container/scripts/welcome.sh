1e2ab647daaf13c2a87dc3ad8747906dad4d7cd1
twaldin <timothy@waldin.net>
2025-08-18 01:51:32 -0700

    small change

--- container/scripts/welcome.sh
+++ container/scripts/welcome.sh
@@ -7,7 +7,7 @@ source "$(dirname "$0")/shared-functions.sh"
 clear
 # ASCII header with progressive typewriter display
 ascii_typewriter "timothy waldin" "Univers" "${BOLD}${CYAN}"
-animated_separator "═" 20
 typewriter "${DIM}Type \"help\" for a list of available commands.${RESET}"
 typewriter "${DIM}Type \"blog\" to see my blog${RESET}"
 typewriter "${DIM}Type \"projects\" to see my projects.${RESET}"
+echo""
